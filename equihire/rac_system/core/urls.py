41b03754a22735b892e4b5edb57be4eb221dce90
param-punjab <mathrooparam@gmail.com>
2025-11-16 10:31:11 +0530

    removing extra files

--- equihire/rac_system/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('home/', views.home, name='home'),
-    path('upload/', views.upload_and_match, name='upload_and_match'),
-    path('download_pdf/', views.download_pdf, name='download_pdf'),  
-]
--- equihire/rac_system/rac_system/info.py
+++
@@ -1,5 +0,0 @@
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'anmolwritesblog@gmail.com'
-EMAIL_HOST_PASSWORD = 'voqu vmrt pxwj lgxp'
-EMAIL_PORT = 587
